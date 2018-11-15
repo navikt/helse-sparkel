45faca2bfa9bceed8ae9a80caf300fc1d96f3cc0
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-15 11:50:35 +0100

    Fixed repo name

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,5 +3,5 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     make release
-    git push --tags https://x-access-token:$INSTALLATION_TOKEN@github.com/navikt/helse-sykepengebehandling.git HEAD:master
+    git push --tags https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLUG.git HEAD:master
 fi
