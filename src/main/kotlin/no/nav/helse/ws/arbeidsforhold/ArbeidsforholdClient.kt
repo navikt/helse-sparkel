ed9dc933ca521bfe8c46ef8621fe08021de355c3
Håkon Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-11-16 09:52:15 +0100

    Added untested SOAP client for AAReg.

---
+++ src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdClient.kt
@@ -0,0 +1,25 @@
+package no.nav.helse.ws.arbeidsforhold
+
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.binding.ArbeidsforholdV3
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Arbeidsforhold
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.NorskIdent
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Regelverker
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.meldinger.FinnArbeidsforholdPrArbeidstakerRequest
+
+class ArbeidsforholdClient(private val arbeidsforhold: ArbeidsforholdV3) {
+
+    fun finnArbeidsforholdForFnr(fnr: String): Collection<Arbeidsforhold> {
+        val request = FinnArbeidsforholdPrArbeidstakerRequest()
+                .apply { ident = NorskIdent().apply { ident = fnr } }
+                .apply { arbeidsforholdIPeriode = null } // optional, håper at null betyr _alle_ arbeidsforhold
+                .apply { rapportertSomRegelverk = Regelverker().apply { kodeverksRef = RegelverkerValues.ALLE.name } }
+
+
+        return arbeidsforhold.finnArbeidsforholdPrArbeidstaker(request)!!.arbeidsforhold!!
+    }
+
+}
+
+enum class RegelverkerValues {
+    FOER_A_ORDNINGEN, A_ORDNINGEN, ALLE
+}
