8cbf26811abeb98e85d9cf349931f141a0c82762
Håkon Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-11-19 14:15:46 +0100

    arbeidsforhold-integration using Success/Failure-pattern and counts results. pretty much copy-paste from PersonClient

--- src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdClient.kt
+++ src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdClient.kt
@@ -1,22 +1,42 @@
 package no.nav.helse.ws.arbeidsforhold
 
+import io.prometheus.client.Counter
+import no.nav.helse.Failure
+import no.nav.helse.OppslagResult
+import no.nav.helse.Success
 import no.nav.helse.ws.Fødselsnummer
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.binding.ArbeidsforholdV3
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Arbeidsforhold
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.NorskIdent
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Regelverker
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.meldinger.FinnArbeidsforholdPrArbeidstakerRequest
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.meldinger.FinnArbeidsforholdPrArbeidstakerResponse
+import org.slf4j.LoggerFactory
 
 class ArbeidsforholdClient(private val arbeidsforhold: ArbeidsforholdV3) {
 
-    fun finnArbeidsforholdForFnr(fnr: Fødselsnummer): Collection<Arbeidsforhold> {
+    private val log = LoggerFactory.getLogger("ArbeidsforholdClient")
+
+    private val counter = Counter.build()
+            .name("oppslag_arbeidsforhold")
+            .labelNames("status")
+            .help("Antall registeroppslag av arbeidsforhold for person")
+            .register()
+
+    fun finnArbeidsforholdForFnr(fnr: Fødselsnummer): OppslagResult {
         val request = FinnArbeidsforholdPrArbeidstakerRequest()
                 .apply { ident = NorskIdent().apply { ident = fnr.value } }
                 .apply { arbeidsforholdIPeriode = null } // optional, håper at null betyr _alle_ arbeidsforhold
                 .apply { rapportertSomRegelverk = Regelverker().apply { kodeverksRef = RegelverkerValues.ALLE.name } }
 
-
-        return arbeidsforhold.finnArbeidsforholdPrArbeidstaker(request)!!.arbeidsforhold!!
+        try {
+            val remoteResult: FinnArbeidsforholdPrArbeidstakerResponse = arbeidsforhold.finnArbeidsforholdPrArbeidstaker(request)
+            counter.labels("success").inc()
+            return Success(remoteResult)
+        } catch (ex: Exception) {
+            log.error("Error while doing arbeidsforhold lookup", ex)
+            counter.labels("failure").inc()
+            return Failure(listOf(ex.message ?: "unknown error"))
+        }
     }
 }
 
--- src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdRoute.kt
+++ src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdRoute.kt
@@ -1,17 +1,23 @@
 package no.nav.helse.ws.arbeidsforhold
 
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.post
+import no.nav.helse.Failure
+import no.nav.helse.OppslagResult
+import no.nav.helse.Success
 import no.nav.helse.ws.Fødselsnummer
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Arbeidsforhold
 
 fun Routing.arbeidsforhold(arbeidsforholdClient: ArbeidsforholdClient) {
     post("api/arbeidsforhold") {
         val fnr = call.receive<String>()
-        val arbeidsforhold: Collection<Arbeidsforhold> = arbeidsforholdClient.finnArbeidsforholdForFnr(Fødselsnummer(fnr))
-        call.respond(arbeidsforhold)
+        val arbeidsforhold: OppslagResult = arbeidsforholdClient.finnArbeidsforholdForFnr(Fødselsnummer(fnr))
+        when (arbeidsforhold) {
+            is Success<*> -> call.respond(arbeidsforhold.data!!)
+            is Failure -> call.respond(HttpStatusCode.InternalServerError, "Could not find arbeidsforhold")
+        }
     }
 }
