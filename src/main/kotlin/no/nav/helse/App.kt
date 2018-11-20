797e38a4942967e71fe089786973ac03c21b7565
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-20 15:06:38 +0100

    Fetch config from env class, and added start and stop methods

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -1,10 +1,7 @@
 package no.nav.helse
 
+import io.grpc.Server
 import io.grpc.ServerBuilder
-import io.grpc.examples.helloworld.GreeterGrpc
-import io.grpc.examples.helloworld.HelloReply
-import io.grpc.examples.helloworld.HelloRequest
-import io.grpc.stub.StreamObserver
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -13,17 +10,19 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.netty.NettyApplicationEngine
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.common.TextFormat
 import io.prometheus.client.hotspot.DefaultExports
 import no.nav.helse.ws.EndpointSTSClientConfig
 import no.nav.helse.ws.WsClientBuilder
-import no.nav.helse.ws.inntekt.InntektClient
-import no.nav.helse.ws.inntekt.inntekt
 import no.nav.helse.ws.arbeidsforhold.ArbeidsforholdClient
 import no.nav.helse.ws.arbeidsforhold.ArbeidsforholdGrpc
 import no.nav.helse.ws.arbeidsforhold.arbeidsforhold
-import no.nav.helse.ws.organisasjon.*
+import no.nav.helse.ws.inntekt.InntektClient
+import no.nav.helse.ws.inntekt.inntekt
+import no.nav.helse.ws.organisasjon.OrganisasjonClient
+import no.nav.helse.ws.organisasjon.organisasjon
 import no.nav.helse.ws.person.PersonClient
 import no.nav.helse.ws.person.person
 import no.nav.helse.ws.sakogbehandling.SakOgBehandlingClient
@@ -32,76 +31,108 @@ import no.nav.helse.ws.sts.STSClientBuilder
 import no.nav.helse.ws.sts.STSProperties
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.binding.ArbeidsforholdV3
 import no.nav.tjeneste.virksomhet.inntekt.v3.binding.InntektV3
-import no.nav.tjeneste.virksomhet.organisasjon.v5.binding.*
+import no.nav.tjeneste.virksomhet.organisasjon.v5.binding.OrganisasjonV5
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.binding.SakOgBehandlingV1
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.util.Collections.emptySet
+import java.util.concurrent.TimeUnit
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
         System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
 
-private val wsClientBuilder = WsClientBuilder()
-private val stsClient = STSClientBuilder().build(STSProperties(
-        URI(getEnvVar("SECURITY_TOKEN_SERVICE_URL")),
-        getEnvVar("SECURITY_TOKEN_SERVICE_USERNAME"),
-        getEnvVar("SECURITY_TOKEN_SERVICE_PASSWORD")
-))
-private val endpointSTSClientConfig = EndpointSTSClientConfig(stsClient)
-
 private val log = LoggerFactory.getLogger("App")
 private val collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
 
+data class Environment(val securityTokenServiceEndpointUrl: String = getEnvVar("SECURITY_TOKEN_SERVICE_URL"),
+                       val securityTokenUsername: String = getEnvVar("SECURITY_TOKEN_SERVICE_USERNAME"),
+                       val securityTokenPassword: String = getEnvVar("SECURITY_TOKEN_SERVICE_PASSWORD"),
+                       val personEndpointUrl: String = getEnvVar("PERSON_ENDPOINTURL"),
+                       val inntektEndpointUrl: String = getEnvVar("INNTEKT_ENDPOINTURL"),
+                       val arbeidsforholdEndpointUrl: String = getEnvVar("AAREG_ENDPOINTURL"),
+                       val organisasjonEndpointUrl: String = getEnvVar("ORGANISASJON_ENDPOINTURL"),
+                       val sakOgBehandlingEndpointUrl: String = getEnvVar("SAK_OG_BEHANDLING_ENDPOINTURL"));
+
 fun main() {
-    DefaultExports.initialize()
+    val env = Environment()
 
-    val personV3 = wsClientBuilder.createPort(getEnvVar("PERSON_ENDPOINTURL"), PersonV3::class.java)
-    endpointSTSClientConfig.configureRequestSamlToken(personV3, EndpointSTSClientConfig.STS_SAML_POLICY)
-    val personClient = PersonClient(personV3)
+    App(env).start()
+}
 
-    val inntektV3 = wsClientBuilder.createPort(getEnvVar("INNTEKT_ENDPOINTURL"), InntektV3::class.java)
-    endpointSTSClientConfig.configureRequestSamlToken(inntektV3, EndpointSTSClientConfig.STS_SAML_POLICY)
-    val inntektClient = InntektClient(inntektV3)
+class App(val env: Environment = Environment()) {
+    private val nettyServer: NettyApplicationEngine
+    private val grpcServer: Server
 
-    val arbeidsforholdV3: ArbeidsforholdV3 = wsClientBuilder.createPort(getEnvVar("AAREG_ENDPOINTURL"), ArbeidsforholdV3::class.java)
-    endpointSTSClientConfig.configureRequestSamlToken(arbeidsforholdV3, EndpointSTSClientConfig.STS_SAML_POLICY)
-    val arbeidsforholdClient = ArbeidsforholdClient(arbeidsforholdV3)
+    init {
+        DefaultExports.initialize()
 
-    val organisasjonV5: OrganisasjonV5 = wsClientBuilder.createPort(getEnvVar("ORGANISASJON_ENDPOINTURL"), OrganisasjonV5::class.java)
-    endpointSTSClientConfig.configureRequestSamlToken(organisasjonV5, EndpointSTSClientConfig.STS_SAML_POLICY)
-    val organisasjonClient = OrganisasjonClient(organisasjonV5)
+        val wsClientBuilder = WsClientBuilder()
+        val stsClient = STSClientBuilder().build(STSProperties(
+                URI(env.securityTokenServiceEndpointUrl),
+                env.securityTokenUsername,
+                env.securityTokenPassword
+        ))
+        val endpointSTSClientConfig = EndpointSTSClientConfig(stsClient)
 
-    val sakOgBehandlingV1: SakOgBehandlingV1 = wsClientBuilder.createPort(getEnvVar("SAK_OG_BEHANDLING_ENDPOINTURL"), SakOgBehandlingV1::class.java)
-    endpointSTSClientConfig.configureRequestSamlToken(sakOgBehandlingV1, EndpointSTSClientConfig.STS_SAML_POLICY)
-    val sakOgBehandlingClient = SakOgBehandlingClient(sakOgBehandlingV1)
+        val personV3 = wsClientBuilder.createPort(env.personEndpointUrl, PersonV3::class.java)
+        endpointSTSClientConfig.configureRequestSamlToken(personV3, EndpointSTSClientConfig.STS_SAML_POLICY)
+        val personClient = PersonClient(personV3)
 
-    embeddedServer(Netty, 8080) {
-        routing {
-            inntekt(inntektClient)
-            person(personClient)
-            arbeidsforhold(arbeidsforholdClient)
-            organisasjon(organisasjonClient)
-            sakOgBehandling(sakOgBehandlingClient)
+        val inntektV3 = wsClientBuilder.createPort(env.inntektEndpointUrl, InntektV3::class.java)
+        endpointSTSClientConfig.configureRequestSamlToken(inntektV3, EndpointSTSClientConfig.STS_SAML_POLICY)
+        val inntektClient = InntektClient(inntektV3)
 
-            get("/isalive") {
-                call.respondText("ALIVE", ContentType.Text.Plain)
-            }
+        val arbeidsforholdV3: ArbeidsforholdV3 = wsClientBuilder.createPort(env.arbeidsforholdEndpointUrl, ArbeidsforholdV3::class.java)
+        endpointSTSClientConfig.configureRequestSamlToken(arbeidsforholdV3, EndpointSTSClientConfig.STS_SAML_POLICY)
+        val arbeidsforholdClient = ArbeidsforholdClient(arbeidsforholdV3)
 
-            get("/isready") {
-                call.respondText("READY", ContentType.Text.Plain)
-            }
+        val organisasjonV5: OrganisasjonV5 = wsClientBuilder.createPort(env.organisasjonEndpointUrl, OrganisasjonV5::class.java)
+        endpointSTSClientConfig.configureRequestSamlToken(organisasjonV5, EndpointSTSClientConfig.STS_SAML_POLICY)
+        val organisasjonClient = OrganisasjonClient(organisasjonV5)
 
-            get("/metrics") {
-                val names = call.request.queryParameters.getAll("name[]")?.toSet() ?: emptySet()
-                call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
-                    TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
+        val sakOgBehandlingV1: SakOgBehandlingV1 = wsClientBuilder.createPort(env.sakOgBehandlingEndpointUrl, SakOgBehandlingV1::class.java)
+        endpointSTSClientConfig.configureRequestSamlToken(sakOgBehandlingV1, EndpointSTSClientConfig.STS_SAML_POLICY)
+        val sakOgBehandlingClient = SakOgBehandlingClient(sakOgBehandlingV1)
+
+        nettyServer = embeddedServer(Netty, 8080) {
+            routing {
+                inntekt(inntektClient)
+                person(personClient)
+                arbeidsforhold(arbeidsforholdClient)
+                organisasjon(organisasjonClient)
+                sakOgBehandling(sakOgBehandlingClient)
+
+                get("/isalive") {
+                    call.respondText("ALIVE", ContentType.Text.Plain)
+                }
+
+                get("/isready") {
+                    call.respondText("READY", ContentType.Text.Plain)
+                }
+
+                get("/metrics") {
+                    val names = call.request.queryParameters.getAll("name[]")?.toSet() ?: emptySet()
+                    call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
+                        TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
+                    }
                 }
             }
         }
-    }.start(wait = false)
 
-    ServerBuilder.forPort(8081)
+        grpcServer = ServerBuilder.forPort(8081)
             .addService(ArbeidsforholdGrpc(arbeidsforholdClient))
-            .build().start()
+            .build()
+    }
+
+    fun start() {
+        nettyServer.start(wait = false)
+        grpcServer.start()
+    }
+
+    fun stop() {
+        grpcServer.shutdownNow()
+        nettyServer.stop(5, 60, TimeUnit.SECONDS)
+        grpcServer.awaitTermination(5, TimeUnit.SECONDS)
+    }
 }
