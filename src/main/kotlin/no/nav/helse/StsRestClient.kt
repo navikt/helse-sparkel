c37fbd972c05819a91fbcc52fa3c318351231c46
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-28 15:00:37 +0100

    Added saml token support

--- src/main/kotlin/no/nav/helse/StsRestClient.kt
+++ src/main/kotlin/no/nav/helse/StsRestClient.kt
@@ -3,24 +3,44 @@ package no.nav.helse
 import com.github.kittinunf.fuel.httpGet
 import org.json.JSONObject
 import java.time.LocalDateTime
+import java.util.*
 
 /**
  * henter jwt token fra STS
  */
 class StsRestClient(val baseUrl: String, val username: String, val password: String) {
-    private var cachedToken: Token? = null
+    private var cachedOidcToken: Token? = null
+    private var cachedSamlToken: Token? = null
 
     fun token(): String {
-        if (Token.shouldRenew(cachedToken))  {
+        if (Token.shouldRenew(cachedOidcToken))  {
             val (_, _, result) = "$baseUrl/rest/v1/sts/token?grant_type=client_credentials&scope=openid".httpGet()
                     .authenticate(username, password)
                     .header(mapOf("Accept" to "application/json"))
                     .responseJSON()
 
-            cachedToken = result.get().mapToToken()
+            cachedOidcToken = result.get().mapToToken()
         }
 
-        return cachedToken!!.accessToken
+        return cachedOidcToken!!.accessToken
+    }
+
+    fun samlToken(): String {
+        if (Token.shouldRenew(cachedSamlToken))  {
+            val (_, _, result) = "$baseUrl/rest/v1/sts/samltoken".httpGet()
+                    .authenticate(username, password)
+                    .header(mapOf("Accept" to "application/json"))
+                    .responseJSON()
+
+            cachedSamlToken = result.get().mapToToken()
+        }
+
+        val urldecodedBase64 = cachedSamlToken!!.accessToken
+                .replace('-', '+')
+                .replace('_', '/')
+                .plus("=".repeat(cachedSamlToken!!.accessToken.length % 4))
+
+        return String(Base64.getDecoder().decode(urldecodedBase64))
     }
 
     private fun JSONObject.mapToToken(): Token {
