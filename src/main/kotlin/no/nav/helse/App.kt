ba39010fc135709417a5758707acaedbd56af91e
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-29 21:22:29 +0100

    Move app setup to Application to help mock app later

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -1,7 +1,9 @@
 package no.nav.helse
 
 import com.auth0.jwk.JwkProviderBuilder
+import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.application.log
 import io.ktor.auth.Authentication
 import io.ktor.auth.authenticate
 import io.ktor.auth.jwt.JWTPrincipal
@@ -14,7 +16,6 @@ import io.ktor.http.ContentType
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.netty.NettyApplicationEngine
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.hotspot.DefaultExports
 import no.nav.helse.nais.nais
@@ -24,91 +25,77 @@ import no.nav.helse.ws.inntekt.inntekt
 import no.nav.helse.ws.organisasjon.organisasjon
 import no.nav.helse.ws.person.person
 import no.nav.helse.ws.sakogbehandling.sakOgBehandling
-import org.slf4j.LoggerFactory
 import org.slf4j.event.Level
 import java.net.URL
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-private val collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
+private val collectorRegistry = CollectorRegistry.defaultRegistry
 
 fun main() {
     val env = Environment()
 
-    val app = App(env)
+    DefaultExports.initialize()
 
-    app.start()
+    val app = embeddedServer(Netty, 8080) {
+        sparkel(env)
+    }
+
+    app.start(wait = false)
 
     Runtime.getRuntime().addShutdownHook(Thread {
-        app.stop()
+        app.stop(5, 60, TimeUnit.SECONDS)
     })
 }
 
-class App(env: Environment = Environment()) {
-    private val nettyServer: NettyApplicationEngine
-    private val clients: Clients = Clients(env)
-
-    private val log = LoggerFactory.getLogger("App")
-
+fun Application.sparkel(env: Environment) {
     val jwkProvider = JwkProviderBuilder(URL(env.jwksUrl))
             .cached(10, 24, TimeUnit.HOURS)
             .rateLimited(10, 1, TimeUnit.MINUTES)
             .build()
 
-    init {
-        DefaultExports.initialize()
+    val clients = Clients(env)
 
-        nettyServer = embeddedServer(Netty, 8080) {
-            install(CallId) {
-                header("Nav-Call-Id")
+    install(CallId) {
+        header("Nav-Call-Id")
 
-                generate { UUID.randomUUID().toString() }
-            }
+        generate { UUID.randomUUID().toString() }
+    }
 
-            install(CallLogging) {
-                level = Level.INFO
-                callIdMdc("call_id")
-            }
+    install(CallLogging) {
+        level = Level.INFO
+        callIdMdc("call_id")
+    }
 
-            install(ContentNegotiation) {
-                register(ContentType.Application.Json, JsonContentConverter())
-            }
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, JsonContentConverter())
+    }
 
-            install(Authentication) {
-                jwt {
-                    verifier(jwkProvider, env.jwtIssuer)
-                    realm = "Helse Sparkel"
-                    validate { credentials ->
-                        log.info("authorization attempt for ${credentials.payload.subject}")
-                        if (credentials.payload.subject in listOf("srvspinne", "srvsplitt")) {
-                            log.info("authorization ok")
-                            return@validate JWTPrincipal(credentials.payload)
-                        }
-                        log.info("authorization failed")
-                        return@validate null
-                    }
+    install(Authentication) {
+        jwt {
+            verifier(jwkProvider, env.jwtIssuer)
+            realm = "Helse Sparkel"
+            validate { credentials ->
+                log.info("authorization attempt for ${credentials.payload.subject}")
+                if (credentials.payload.subject in listOf("srvspinne", "srvsplitt")) {
+                    log.info("authorization ok")
+                    return@validate JWTPrincipal(credentials.payload)
                 }
-            }
-
-            routing {
-                authenticate {
-                    inntekt(clients.inntektClient)
-                    person(clients.personClient)
-                    arbeidsforhold(clients.arbeidsforholdClient)
-                    organisasjon(clients.organisasjonClient)
-                    sakOgBehandling(clients.sakOgBehandlingClient)
-                }
-
-                nais(collectorRegistry)
+                log.info("authorization failed")
+                return@validate null
             }
         }
     }
 
-    fun start() {
-        nettyServer.start(wait = false)
-    }
+    routing {
+        authenticate {
+            inntekt(clients.inntektClient)
+            person(clients.personClient)
+            arbeidsforhold(clients.arbeidsforholdClient)
+            organisasjon(clients.organisasjonClient)
+            sakOgBehandling(clients.sakOgBehandlingClient)
+        }
 
-    fun stop() {
-        nettyServer.stop(5, 60, TimeUnit.SECONDS)
+        nais(collectorRegistry)
     }
 }
