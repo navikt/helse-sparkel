efb7dc049090326c0ab03391a26b9a11958b1360
Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-12-20 10:41:38 +0100

    data-minimizeish; parse sykepengerListeResponse to a list of only the data we are looking for. Did not find amount, though...

--- src/main/kotlin/no/nav/helse/ws/sykepenger/SykepengeClient.kt
+++ src/main/kotlin/no/nav/helse/ws/sykepenger/SykepengeClient.kt
@@ -6,10 +6,14 @@ import no.nav.helse.OppslagResult
 import no.nav.helse.Success
 import no.nav.tjeneste.virksomhet.sykepenger.v2.binding.SykepengerV2
 import no.nav.tjeneste.virksomhet.sykepenger.v2.informasjon.Periode
+import no.nav.tjeneste.virksomhet.sykepenger.v2.informasjon.Sykmeldingsperiode
+import no.nav.tjeneste.virksomhet.sykepenger.v2.informasjon.Vedtak
 import no.nav.tjeneste.virksomhet.sykepenger.v2.meldinger.HentSykepengerListeRequest
 import no.nav.tjeneste.virksomhet.sykepenger.v2.meldinger.HentSykepengerListeResponse
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
+import java.math.BigDecimal
+import java.time.LocalDate
 import javax.xml.datatype.DatatypeFactory
 import javax.xml.datatype.XMLGregorianCalendar
 
@@ -25,11 +29,10 @@ class SykepengerClient(private val sykepenger: SykepengerV2) {
 
     fun finnSykepengeVedtak(aktorId: String, fraOgMed: DateTime, tilOgMed: DateTime): OppslagResult {
         val request = createSykepengerListeRequest(aktorId, fraOgMed, tilOgMed)
-
         return try {
             val remoteResult: HentSykepengerListeResponse? = sykepenger.hentSykepengerListe(request)
             counter.labels("success").inc()
-            Success(remoteResult)
+            Success(remoteResult?.toSykepengerVedtak(aktorId))
         } catch (ex: Exception) {
             log.error("Error while doing sak og behndling lookup", ex)
             counter.labels("failure").inc()
@@ -51,4 +54,36 @@ class SykepengerClient(private val sykepenger: SykepengerV2) {
     fun toCal(dt: DateTime): XMLGregorianCalendar {
         return DatatypeFactory.newInstance().newXMLGregorianCalendar(dt.toGregorianCalendar())
     }
+}
+
+data class SykepengerVedtak(val fom: LocalDate,
+                            val tom: LocalDate,
+                            val grad: Float,
+                            val mottaker: String,
+                            val beløp: BigDecimal = BigDecimal.ZERO) // <- I can't find where to get this
+
+fun HentSykepengerListeResponse.toSykepengerVedtak(aktorId: String): Collection<SykepengerVedtak> {
+    return when {
+        this.sykmeldingsperiodeListe.isEmpty() -> emptyList()
+        else -> this.sykmeldingsperiodeListe.flatMap { it.toSykepengerVedtak(aktorId) }
+    }
+}
+
+fun Sykmeldingsperiode.toSykepengerVedtak(aktorId: String): Collection<SykepengerVedtak> {
+    return when {
+        this.vedtakListe.isEmpty() -> emptyList<SykepengerVedtak>()
+        else -> this.vedtakListe.map { it.toSykepengerVedtak(aktorId) }
+    }
+}
+
+fun Vedtak.toSykepengerVedtak(aktorId: String): SykepengerVedtak {
+    return SykepengerVedtak(fom = this.vedtak.fom.toLocalDate(),
+            tom = this.vedtak.tom.toLocalDate(),
+            beløp = BigDecimal.ONE,
+            mottaker = aktorId,
+            grad = this.utbetalingsgrad.toFloat())
+}
+
+fun XMLGregorianCalendar.toLocalDate(): LocalDate {
+    return LocalDate.of(this.year, this.month, this.day)
 }
