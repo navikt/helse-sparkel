dca28a585e514b74010edc956fa70d4af7bdb5dd
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-01-29 10:59:03 +0100

    Added YearMonth to XMLGregorianCalendar converter

--- src/main/kotlin/no/nav/helse/common/Datetime.kt
+++ src/main/kotlin/no/nav/helse/common/Datetime.kt
@@ -1,8 +1,11 @@
 package no.nav.helse.common
 
-import java.time.*
-import java.util.*
-import javax.xml.datatype.*
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.ZoneId
+import java.util.GregorianCalendar
+import javax.xml.datatype.DatatypeFactory
+import javax.xml.datatype.XMLGregorianCalendar
 
 private val datatypeFactory = DatatypeFactory.newInstance()
 
@@ -12,3 +15,5 @@ fun LocalDate.toXmlGregorianCalendar() = this.let {
     val gcal = GregorianCalendar.from(this.atStartOfDay(ZoneId.systemDefault()))
     datatypeFactory.newXMLGregorianCalendar(gcal)
 }
+
+fun YearMonth.toXmlGregorianCalendar() = atDay(1).toXmlGregorianCalendar()
--- src/test/kotlin/no/nav/helse/common/DatetimeTests.kt
+++ src/test/kotlin/no/nav/helse/common/DatetimeTests.kt
@@ -1,10 +1,11 @@
 package no.nav.helse.common
 
-import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.*
-import java.time.*
-import java.util.*
-import javax.xml.datatype.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.YearMonth
+import java.util.TimeZone
+import javax.xml.datatype.DatatypeFactory
 
 class DatetimeTests {
 
@@ -35,4 +36,19 @@ class DatetimeTests {
         assertEquals(expected, actual)
     }
 
+    @Test
+    fun toCalendarFromYearMonth() {
+        val expected = DatatypeFactory.newInstance().newXMLGregorianCalendar().apply {
+            year = 2019
+            month = 1
+            day = 1
+            hour = 0
+            minute = 0
+            second = 0
+            millisecond = 0
+            timezone = TimeZone.getDefault().rawOffset / 1000 / 60
+        }
+        val actual = YearMonth.of(2019, 1).toXmlGregorianCalendar()
+        assertEquals(expected, actual)
+    }
 }
