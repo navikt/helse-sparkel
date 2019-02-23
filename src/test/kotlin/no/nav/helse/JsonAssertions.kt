453cb3d681c3b079387afbb2e3f3665b54c56e0e
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-23 18:11:36 +0100

    Added descriptive error messages

--- src/test/kotlin/no/nav/helse/JsonAssertions.kt
+++ src/test/kotlin/no/nav/helse/JsonAssertions.kt
@@ -5,16 +5,16 @@ import org.json.JSONObject
 import org.junit.jupiter.api.Assertions
 
 fun assertJsonEquals(expected: JSONObject, actual: JSONObject) {
-    Assertions.assertEquals(expected.length(), actual.length())
+    Assertions.assertEquals(expected.length(), actual.length(), "$actual does not match $expected")
 
     expected.keys().forEach {
-        Assertions.assertTrue(actual.has(it))
+        Assertions.assertTrue(actual.has(it), "$it is not present in $actual")
 
         val expectedValue = expected.get(it)
         when(expectedValue) {
             is JSONObject -> assertJsonEquals(expectedValue, actual.get(it) as JSONObject)
             is JSONArray -> assertJsonEquals(expectedValue, actual.get(it) as JSONArray)
-            else -> Assertions.assertEquals(expectedValue, actual.get(it))
+            else -> Assertions.assertEquals(expectedValue, actual.get(it), "${actual.get(it)} does not match $expectedValue")
         }
     }
 }
@@ -26,7 +26,7 @@ fun assertJsonEquals(expected: JSONArray, actual: JSONArray) {
         when(it) {
             is JSONObject -> assertJsonEquals(it, actual.get(index) as JSONObject)
             is JSONArray -> assertJsonEquals(it, actual.get(index) as JSONArray)
-            else -> Assertions.assertEquals(it, actual.get(index))
+            else -> Assertions.assertEquals(it, actual.get(index), "${actual.get(index)} does not match $it")
         }
     }
 }
