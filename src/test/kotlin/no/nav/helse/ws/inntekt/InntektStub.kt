af40e2725bd807afbaab50b365c5d17ca10914ad
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-23 14:05:25 +0100

    Renamed component test to integration test

--- src/test/kotlin/no/nav/helse/ws/inntekt/InntektStub.kt
+++ src/test/kotlin/no/nav/helse/ws/inntekt/InntektStub.kt
@@ -5,15 +5,15 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.matching.MatchesXPathPattern
 import no.nav.helse.ws.withSoapAction
 
-fun hentInntektListeBolkStub(ident: String, månedFom: String, månedTom: String): MappingBuilder {
+fun hentInntektListeBolkStub(ident: String, månedFom: String, månedTom: String, filter: String, formål: String): MappingBuilder {
     return WireMock.post("/inntekt")
             .withSoapAction("http://nav.no/tjeneste/virksomhet/inntekt/v3/Inntekt_v3/hentInntektListeBolkRequest")
             .withRequestBody(MatchesXPathPattern("//soap:Envelope/soap:Body/v3:hentInntektListeBolk/request/identListe/personIdent/text()",
                     inntektNamespace, WireMock.equalTo(ident)))
             .withRequestBody(MatchesXPathPattern("//soap:Envelope/soap:Body/v3:hentInntektListeBolk/request/ainntektsfilter/text()",
-                    inntektNamespace, WireMock.equalTo("ForeldrepengerA-Inntekt")))
+                    inntektNamespace, WireMock.equalTo(filter)))
             .withRequestBody(MatchesXPathPattern("//soap:Envelope/soap:Body/v3:hentInntektListeBolk/request/formaal/text()",
-                    inntektNamespace, WireMock.equalTo("Foreldrepenger")))
+                    inntektNamespace, WireMock.equalTo(formål)))
             .withRequestBody(MatchesXPathPattern("//soap:Envelope/soap:Body/v3:hentInntektListeBolk/request/uttrekksperiode/maanedFom/text()",
                     inntektNamespace, WireMock.equalTo(månedFom)))
             .withRequestBody(MatchesXPathPattern("//soap:Envelope/soap:Body/v3:hentInntektListeBolk/request/uttrekksperiode/maanedTom/text()",
