cec41743932b401bd8d603f285f00d991bd45e39
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-24 11:55:50 +0100

    Rewrote GeografiskTilknytningComponentTest without WireMock. Using WireMock to create a test double is slow, and should be restricted to integration tests

--- src/test/kotlin/no/nav/helse/ws/person/GeografiskTilknytningComponentTest.kt
+++ src/test/kotlin/no/nav/helse/ws/person/GeografiskTilknytningComponentTest.kt
@@ -1,203 +1,290 @@
 package no.nav.helse.ws.person
 
-import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.helse.JwtStub
 import no.nav.helse.assertJsonEquals
-import no.nav.helse.bootstrapComponentTest
 import no.nav.helse.mockedSparkel
-import no.nav.helse.sts.StsRestClient
-import no.nav.helse.ws.WsClients
-import no.nav.helse.ws.person.GeografiskTilknytningMocks.medDiskresjonsKode6Responses
-import no.nav.helse.ws.person.GeografiskTilknytningMocks.medDiskresjonsKode7Responses
-import no.nav.helse.ws.person.GeografiskTilknytningMocks.medGeografiskTilknytningResponses
-import no.nav.helse.ws.person.GeografiskTilknytningMocks.mock
-import no.nav.helse.ws.person.GeografiskTilknytningMocks.utenGeografiskTilknytningEllerDiskresjonskode
-import no.nav.helse.ws.sts.stsClient
+import no.nav.helse.ws.AktørId
+import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.AktoerId
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Bydel
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Diskresjonskoder
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Kommune
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Land
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Personnavn
+import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentGeografiskTilknytningResponse
 import org.json.JSONObject
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class GeografiskTilknytningComponentTest {
 
-    companion object {
-        val bootstrap = bootstrapComponentTest()
-        val token = bootstrap.jwkStub.createTokenFor("srvpleiepenger-opp")
-
-        @BeforeAll
-        @JvmStatic
-        fun start() {
-            bootstrap.start()
-        }
-
-        @AfterAll
-        @JvmStatic
-        fun stop() {
-            bootstrap.stop()
-        }
-    }
-
-    @BeforeEach
-    @AfterEach
-    fun `reset`() {
-        bootstrap.reset()
-    }
-
     @Test
     fun `Geografisk Tilknytning på en person med kode 6`() {
-        val aktoerId = "1831212532188"
-        val responses = medDiskresjonsKode6Responses(aktoerId = aktoerId)
+        val aktørId = AktørId("1234567891011")
 
-        mock(aktoerId = aktoerId, xml = responses.registerXmlResponse)
+        val personV3 = mockk<PersonV3>()
+        every {
+            personV3.hentGeografiskTilknytning(match {
+                (it.aktoer as AktoerId).aktoerId == aktørId.aktor
+            })
+        } returns HentGeografiskTilknytningResponse().apply {
+            aktoer = AktoerId().apply {
+                aktoerId = aktørId.aktor
+            }
+            navn = Personnavn().apply {
+                etternavn = "DAME"
+                mellomnavn = "SKJERMET"
+                sammensattNavn = "DAME SKJERMET"
+            }
+            diskresjonskode = Diskresjonskoder().apply {
+                value = "SPSF"
+            }
+        }
 
-        requestSparkelAndAssertResponse(
-                aktoerId = aktoerId,
-                expectedResponse = responses.sparkelJsonResponse,
-                expectedHttpResponseCode = 403
-        )
+        val jwkStub = JwtStub("test issuer")
+        val token = jwkStub.createTokenFor("srvpleiepengesokna")
+
+        withTestApplication({mockedSparkel(
+                jwtIssuer = "test issuer",
+                jwkProvider = jwkStub.stubbedJwkProvider(),
+                personService = PersonService(PersonClient(personV3))
+        )}) {
+            handleRequest(HttpMethod.Get, "/api/person/${aktørId.aktor}/geografisk-tilknytning") {
+                addHeader(HttpHeaders.Authorization, "Bearer ${token}")
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.Forbidden.value, response.status()?.value)
+                assertJsonEquals(JSONObject(expected_geografisk_tilknytning_kode_6_response), JSONObject(response.content))
+            }
+        }
     }
 
     @Test
     fun `Geografisk Tilknytning på en person med kode 7`() {
-        val aktoerId = "1831212532189"
-        val geografiskOmraadeType = "Land"
-        val geografiskOmraadeKode = "030155"
+        val aktørId = AktørId("1234567891011")
 
-        val responses = medDiskresjonsKode7Responses(
-                aktoerId = aktoerId,
-                geografiskOmraadeType = geografiskOmraadeType,
-                geografiskOmraadeKode = geografiskOmraadeKode
-        )
+        val personV3 = mockk<PersonV3>()
+        every {
+            personV3.hentGeografiskTilknytning(match {
+                (it.aktoer as AktoerId).aktoerId == aktørId.aktor
+            })
+        } returns HentGeografiskTilknytningResponse().apply {
+            aktoer = AktoerId().apply {
+                aktoerId = aktørId.aktor
+            }
+            navn = Personnavn().apply {
+                etternavn = "DORULL"
+                mellomnavn = "DORULL STOR"
+                sammensattNavn = "STOR"
+            }
+            diskresjonskode = Diskresjonskoder().apply {
+                value = "SPFO"
+            }
+            geografiskTilknytning = Land().apply {
+                geografiskTilknytning = "030155"
+            }
+        }
 
-        mock(aktoerId = aktoerId, xml = responses.registerXmlResponse)
+        val jwkStub = JwtStub("test issuer")
+        val token = jwkStub.createTokenFor("srvpleiepengesokna")
 
-        requestSparkelAndAssertResponse(
-                aktoerId = aktoerId,
-                expectedResponse = responses.sparkelJsonResponse
-        )
+        withTestApplication({mockedSparkel(
+                jwtIssuer = "test issuer",
+                jwkProvider = jwkStub.stubbedJwkProvider(),
+                personService = PersonService(PersonClient(personV3))
+        )}) {
+            handleRequest(HttpMethod.Get, "/api/person/${aktørId.aktor}/geografisk-tilknytning") {
+                addHeader(HttpHeaders.Authorization, "Bearer ${token}")
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.OK.value, response.status()?.value)
+                assertJsonEquals(JSONObject(expected_geografisk_tilknytning_response("Land", "030155")), JSONObject(response.content))
+            }
+        }
     }
 
     @Test
     fun `Geografisk Tilknytning på en person registrert på land`() {
-        val aktoerId = "1831212532190"
-        val geografiskOmraadeType = "Land"
-        val geografiskOmraadeKode = "030103"
+        val aktørId = AktørId("1234567891011")
 
-        val responses = medGeografiskTilknytningResponses(
-                aktoerId = aktoerId,
-                geografiskOmraadeType = geografiskOmraadeType,
-                geografiskOmraadeKode = geografiskOmraadeKode
-        )
+        val personV3 = mockk<PersonV3>()
+        every {
+            personV3.hentGeografiskTilknytning(match {
+                (it.aktoer as AktoerId).aktoerId == aktørId.aktor
+            })
+        } returns HentGeografiskTilknytningResponse().apply {
+            aktoer = AktoerId().apply {
+                aktoerId = aktørId.aktor
+            }
+            navn = Personnavn().apply {
+                etternavn = "LOLNES"
+                mellomnavn = "PIKENES"
+                fornavn = "JENNY"
+                sammensattNavn = "LOLNES JENNY PIKENES"
+            }
+            geografiskTilknytning = Land().apply {
+                geografiskTilknytning = "030103"
+            }
+        }
 
-        mock(
-                aktoerId = aktoerId,
-                xml = responses.registerXmlResponse
-        )
+        val jwkStub = JwtStub("test issuer")
+        val token = jwkStub.createTokenFor("srvpleiepengesokna")
 
-        requestSparkelAndAssertResponse(
-                aktoerId = aktoerId,
-                expectedResponse = responses.sparkelJsonResponse
-        )
+        withTestApplication({mockedSparkel(
+                jwtIssuer = "test issuer",
+                jwkProvider = jwkStub.stubbedJwkProvider(),
+                personService = PersonService(PersonClient(personV3))
+        )}) {
+            handleRequest(HttpMethod.Get, "/api/person/${aktørId.aktor}/geografisk-tilknytning") {
+                addHeader(HttpHeaders.Authorization, "Bearer ${token}")
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.OK.value, response.status()?.value)
+                assertJsonEquals(JSONObject(expected_geografisk_tilknytning_response("Land", "030103")), JSONObject(response.content))
+            }
+        }
     }
 
     @Test
     fun `Geografisk Tilknytning på en person registrert på kommune`() {
-        val aktoerId = "1831212532191"
-        val geografiskOmraadeType = "Kommune"
-        val geografiskOmraadeKode = "030104"
+        val aktørId = AktørId("1234567891011")
 
-        val responses = medGeografiskTilknytningResponses(
-                aktoerId = aktoerId,
-                geografiskOmraadeType = geografiskOmraadeType,
-                geografiskOmraadeKode = geografiskOmraadeKode
-        )
+        val personV3 = mockk<PersonV3>()
+        every {
+            personV3.hentGeografiskTilknytning(match {
+                (it.aktoer as AktoerId).aktoerId == aktørId.aktor
+            })
+        } returns HentGeografiskTilknytningResponse().apply {
+            aktoer = AktoerId().apply {
+                aktoerId = aktørId.aktor
+            }
+            navn = Personnavn().apply {
+                etternavn = "LOLNES"
+                mellomnavn = "PIKENES"
+                fornavn = "JENNY"
+                sammensattNavn = "LOLNES JENNY PIKENES"
+            }
+            geografiskTilknytning = Kommune().apply {
+                geografiskTilknytning = "030104"
+            }
+        }
 
-        mock(
-                aktoerId = aktoerId,
-                xml = responses.registerXmlResponse
-        )
+        val jwkStub = JwtStub("test issuer")
+        val token = jwkStub.createTokenFor("srvpleiepengesokna")
 
-        requestSparkelAndAssertResponse(
-                aktoerId = aktoerId,
-                expectedResponse = responses.sparkelJsonResponse
-        )
-
+        withTestApplication({mockedSparkel(
+                jwtIssuer = "test issuer",
+                jwkProvider = jwkStub.stubbedJwkProvider(),
+                personService = PersonService(PersonClient(personV3))
+        )}) {
+            handleRequest(HttpMethod.Get, "/api/person/${aktørId.aktor}/geografisk-tilknytning") {
+                addHeader(HttpHeaders.Authorization, "Bearer ${token}")
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.OK.value, response.status()?.value)
+                assertJsonEquals(JSONObject(expected_geografisk_tilknytning_response("Kommune", "030104")), JSONObject(response.content))
+            }
+        }
     }
 
     @Test
     fun `Geografisk Tilknytning på en person registrert på bydel`() {
-        val aktoerId = "1831212532192"
-        val geografiskOmraadeType = "Bydel"
-        val geografiskOmraadeKode = "030105"
+        val aktørId = AktørId("1234567891011")
 
-        val responses = medGeografiskTilknytningResponses(
-                aktoerId = aktoerId,
-                geografiskOmraadeType = geografiskOmraadeType,
-                geografiskOmraadeKode = geografiskOmraadeKode
-        )
+        val personV3 = mockk<PersonV3>()
+        every {
+            personV3.hentGeografiskTilknytning(match {
+                (it.aktoer as AktoerId).aktoerId == aktørId.aktor
+            })
+        } returns HentGeografiskTilknytningResponse().apply {
+            aktoer = AktoerId().apply {
+                aktoerId = aktørId.aktor
+            }
+            navn = Personnavn().apply {
+                etternavn = "LOLNES"
+                mellomnavn = "PIKENES"
+                fornavn = "JENNY"
+                sammensattNavn = "LOLNES JENNY PIKENES"
+            }
+            geografiskTilknytning = Bydel().apply {
+                geografiskTilknytning = "030105"
+            }
+        }
 
-        mock(
-                aktoerId = aktoerId,
-                xml = responses.registerXmlResponse
-        )
+        val jwkStub = JwtStub("test issuer")
+        val token = jwkStub.createTokenFor("srvpleiepengesokna")
 
-        requestSparkelAndAssertResponse(
-                aktoerId = aktoerId,
-                expectedResponse = responses.sparkelJsonResponse
-        )
+        withTestApplication({mockedSparkel(
+                jwtIssuer = "test issuer",
+                jwkProvider = jwkStub.stubbedJwkProvider(),
+                personService = PersonService(PersonClient(personV3))
+        )}) {
+            handleRequest(HttpMethod.Get, "/api/person/${aktørId.aktor}/geografisk-tilknytning") {
+                addHeader(HttpHeaders.Authorization, "Bearer ${token}")
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.OK.value, response.status()?.value)
+                assertJsonEquals(JSONObject(expected_geografisk_tilknytning_response("Bydel", "030105")), JSONObject(response.content))
+            }
+        }
     }
 
     @Test
     fun `Person uten Geografisk Tilnytning`() {
-        val aktoerId = "1831212532193"
+        val aktørId = AktørId("1234567891011")
 
-        val responses = utenGeografiskTilknytningEllerDiskresjonskode(
-                aktoerId = aktoerId
-        )
+        val personV3 = mockk<PersonV3>()
+        every {
+            personV3.hentGeografiskTilknytning(match {
+                (it.aktoer as AktoerId).aktoerId == aktørId.aktor
+            })
+        } returns HentGeografiskTilknytningResponse().apply {
+            aktoer = AktoerId().apply {
+                aktoerId = aktørId.aktor
+            }
+            navn = Personnavn().apply {
+                etternavn = "LOLNES"
+                mellomnavn = "PIKENES"
+                fornavn = "JENNY"
+                sammensattNavn = "LOLNES JENNY PIKENES"
+            }
+        }
 
-        mock(
-                aktoerId = aktoerId,
-                xml = responses.registerXmlResponse
-        )
-
-        requestSparkelAndAssertResponse(
-                aktoerId = aktoerId,
-                expectedResponse = responses.sparkelJsonResponse,
-                expectedHttpResponseCode = 404
-        )
-    }
-
-
-    private fun requestSparkelAndAssertResponse(
-            aktoerId: String,
-            expectedResponse : String,
-            expectedHttpResponseCode : Int = 200
-    ) {
-        val stsClientWs = stsClient(bootstrap.env.securityTokenServiceEndpointUrl,
-                bootstrap.env.securityTokenUsername to bootstrap.env.securityTokenPassword)
-        val stsClientRest = StsRestClient(
-                bootstrap.env.stsRestUrl, bootstrap.env.securityTokenUsername, bootstrap.env.securityTokenPassword)
-
-        val wsClients = WsClients(stsClientWs, stsClientRest, bootstrap.env.allowInsecureSoapRequests)
+        val jwkStub = JwtStub("test issuer")
+        val token = jwkStub.createTokenFor("srvpleiepengesokna")
 
         withTestApplication({mockedSparkel(
-                jwtIssuer = bootstrap.env.jwtIssuer,
-                jwkProvider = bootstrap.jwkStub.stubbedJwkProvider(),
-                personService = PersonService(wsClients.person(bootstrap.env.personEndpointUrl))
+                jwtIssuer = "test issuer",
+                jwkProvider = jwkStub.stubbedJwkProvider(),
+                personService = PersonService(PersonClient(personV3))
         )}) {
-            handleRequest(HttpMethod.Get, "/api/person/$aktoerId/geografisk-tilknytning") {
-                addHeader(HttpHeaders.Accept, ContentType.Application.Json.toString())
-                addHeader(HttpHeaders.Authorization, "Bearer $token")
+            handleRequest(HttpMethod.Get, "/api/person/${aktørId.aktor}/geografisk-tilknytning") {
+                addHeader(HttpHeaders.Authorization, "Bearer ${token}")
             }.apply {
-                assertEquals(expectedHttpResponseCode, response.status()?.value)
-                assertJsonEquals(JSONObject(expectedResponse), JSONObject(response.content))
+                Assertions.assertEquals(HttpStatusCode.NotFound.value, response.status()?.value)
+                assertJsonEquals(JSONObject(expected_geografisk_tilknytning_empty_response), JSONObject(response.content))
             }
         }
     }
 }
+
+private val expected_geografisk_tilknytning_kode_6_response = """
+{
+    "error": "Ikke tilgang til å se geografisk tilknytning til denne aktøren."
+}
+""".trimIndent()
+
+private fun expected_geografisk_tilknytning_response(type: String, kode: String) = """
+{
+    "type": "${type.toUpperCase()}",
+    "kode": "$kode"
+}
+""".trimIndent()
+
+private val expected_geografisk_tilknytning_empty_response = """
+{
+    "error": "Aktøren har ingen geografisk tilknytning."
+}
+""".trimIndent()
