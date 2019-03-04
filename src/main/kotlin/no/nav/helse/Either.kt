7afc07060022e8680006b728409c23618df8677b
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-03-04 12:52:12 +0100

    move feilårsak to own file

--- src/main/kotlin/no/nav/helse/Either.kt
+++ src/main/kotlin/no/nav/helse/Either.kt
@@ -1,33 +1,5 @@
 package no.nav.helse
 
-import io.ktor.application.ApplicationCall
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
-
-data class FeilResponse(val feilmelding: String)
-data class HttpFeil(val status: HttpStatusCode, val feilmelding: String)
-
-suspend fun ApplicationCall.respondFeil(feil: HttpFeil) = respond(feil.status, FeilResponse(feil.feilmelding))
-
-suspend fun <B: Any> Either<Feilårsak, B>.respond(call: ApplicationCall) = when (this) {
-    is Either.Right -> call.respond(right)
-    is Either.Left -> call.respondFeil(left.toHttpFeil())
-}
-
-sealed class Feilårsak {
-    object IkkeFunnet: Feilårsak()
-    object FeilFraTjeneste: Feilårsak()
-    object UkjentFeil: Feilårsak()
-    object IkkeImplementert: Feilårsak()
-}
-
-fun Feilårsak.toHttpFeil() = when (this) {
-    is Feilårsak.IkkeFunnet -> HttpFeil(HttpStatusCode.NotFound, "Resource not found")
-    is Feilårsak.FeilFraTjeneste -> HttpFeil(HttpStatusCode.InternalServerError, "Error while contacting external service")
-    is Feilårsak.UkjentFeil -> HttpFeil(HttpStatusCode.InternalServerError, "Unknown error")
-    is Feilårsak.IkkeImplementert -> HttpFeil(HttpStatusCode.NotImplemented, "Not implemented")
-}
-
 sealed class Either<out A, out R> {
 
     data class Left<out A>(val left: A) : Either<A, Nothing>()
