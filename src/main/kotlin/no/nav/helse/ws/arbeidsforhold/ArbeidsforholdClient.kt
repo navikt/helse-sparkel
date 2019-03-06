428c6a7c9bd243e2ec81b15d4a707035af41be1a
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-03-06 09:08:08 +0100

    remove unnecessary function

--- src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdClient.kt
+++ src/main/kotlin/no/nav/helse/ws/arbeidsforhold/ArbeidsforholdClient.kt
@@ -2,14 +2,13 @@ package no.nav.helse.ws.arbeidsforhold
 
 import no.nav.helse.Either
 import no.nav.helse.common.toXmlGregorianCalendar
-import no.nav.helse.map
 import no.nav.helse.ws.AktørId
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.binding.ArbeidsforholdV3
+import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Arbeidsforhold
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.NorskIdent
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Periode
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.informasjon.arbeidsforhold.Regelverker
 import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.meldinger.FinnArbeidsforholdPrArbeidstakerRequest
-import no.nav.tjeneste.virksomhet.arbeidsforhold.v3.meldinger.FinnArbeidsforholdPrArbeidstakerResponse
 import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
@@ -17,20 +16,12 @@ class ArbeidsforholdClient(private val arbeidsforholdV3: ArbeidsforholdV3) {
 
     private val log = LoggerFactory.getLogger("ArbeidsforholdClient")
 
-    fun finnArbeidsforhold(aktørId: AktørId, fom: LocalDate, tom: LocalDate) =
-            finnArbeidsforholdForFnr(aktørId, fom, tom).map { arbeidsforholdResponse ->
-                arbeidsforholdResponse.arbeidsforhold!!
-                        .map { arbeidsforhold ->
-                            arbeidsforhold!!
-                        }
-            }.map {
-                it.toList()
-            }
-
-    private fun finnArbeidsforholdForFnr(aktørId: AktørId, fom: LocalDate, tom: LocalDate): Either<Exception, FinnArbeidsforholdPrArbeidstakerResponse> {
+    fun finnArbeidsforhold(aktørId: AktørId, fom: LocalDate, tom: LocalDate): Either<Exception, List<Arbeidsforhold>> {
         val request = FinnArbeidsforholdPrArbeidstakerRequest()
                 .apply {
-                    ident = NorskIdent().apply { ident = aktørId.aktor }
+                    ident = NorskIdent().apply {
+                        ident = aktørId.aktor
+                    }
                     arbeidsforholdIPeriode = Periode().apply {
                         this.fom = fom.toXmlGregorianCalendar()
                         this.tom = tom.toXmlGregorianCalendar()
@@ -41,7 +32,7 @@ class ArbeidsforholdClient(private val arbeidsforholdV3: ArbeidsforholdV3) {
                     }
                 }
         return try {
-            Either.Right(arbeidsforholdV3.finnArbeidsforholdPrArbeidstaker(request))
+            Either.Right(arbeidsforholdV3.finnArbeidsforholdPrArbeidstaker(request).arbeidsforhold.toList())
         } catch (ex: Exception) {
             log.error("Error while doing arbeidsforhold lookup", ex)
             Either.Left(ex)
