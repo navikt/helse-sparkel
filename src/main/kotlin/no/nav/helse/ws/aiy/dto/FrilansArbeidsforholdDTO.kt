df8331eba265c72275b81aaecae172eaed9184dc
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-04-09 14:00:58 +0200

    yrke can be null

--- src/main/kotlin/no/nav/helse/ws/aiy/dto/FrilansArbeidsforholdDTO.kt
+++ src/main/kotlin/no/nav/helse/ws/aiy/dto/FrilansArbeidsforholdDTO.kt
@@ -2,4 +2,4 @@ package no.nav.helse.ws.aiy.dto
 
 import java.time.LocalDate
 
-data class FrilansArbeidsforholdDTO(val arbeidsgiver: no.nav.helse.ws.inntekt.dto.ArbeidsgiverDTO, val yrke: String, val startdato: LocalDate? = null, val sluttdato: LocalDate? = null)
+data class FrilansArbeidsforholdDTO(val arbeidsgiver: no.nav.helse.ws.inntekt.dto.ArbeidsgiverDTO, val yrke: String?, val startdato: LocalDate? = null, val sluttdato: LocalDate? = null)
