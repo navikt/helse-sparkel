27bb63fc89ed8e4d84c6fcd8660c5e1db66d6aa8
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-05-03 14:01:53 +0200

    ha med permisjonsprosent i feilmeldingen

--- src/main/kotlin/no/nav/helse/domene/arbeid/domain/Permisjon.kt
+++ src/main/kotlin/no/nav/helse/domene/arbeid/domain/Permisjon.kt
@@ -9,7 +9,7 @@ data class Permisjon(val fom: LocalDate,
                      val årsak: String) {
     init {
         if (permisjonsprosent < BigDecimal.ZERO || permisjonsprosent > BigDecimal(100)) {
-            throw IllegalArgumentException("permisjonsprosent må være i intervallet [0, 100]")
+            throw IllegalArgumentException("permisjonsprosent $permisjonsprosent må være i intervallet [0, 100]")
         }
         if (tom != null && fom > tom) {
             throw IllegalArgumentException("fom er større enn tom")
