0e926cc3a7a45298eb11f3f905a54f06144f9b64
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-05-21 15:48:58 +0200

    endepunkt for fnr->aktør

--- src/main/kotlin/no/nav/helse/domene/aktør/AktørregisterRoute.kt
+++ src/main/kotlin/no/nav/helse/domene/aktør/AktørregisterRoute.kt
@@ -3,11 +3,15 @@ package no.nav.helse.domene.aktør
 import io.ktor.application.call
 import io.ktor.routing.Route
 import io.ktor.routing.get
-import no.nav.helse.respond
 import no.nav.helse.domene.AktørId
+import no.nav.helse.domene.Fødselsnummer
+import no.nav.helse.respond
 
 fun Route.fnrForAktør(aktørregisterService: AktørregisterService) {
     get("api/aktor/{aktorId}/fnr") {
         aktørregisterService.fødselsnummerForAktør(AktørId(call.parameters["aktorId"]!!)).respond(call)
+    }
+    get("api/aktor/{fnr}/aktor") {
+        aktørregisterService.aktørForFødselsnummer(Fødselsnummer(call.parameters["fnr"]!!)).respond(call)
     }
 }
--- src/main/kotlin/no/nav/helse/domene/aktør/AktørregisterService.kt
+++ src/main/kotlin/no/nav/helse/domene/aktør/AktørregisterService.kt
@@ -1,13 +1,18 @@
 package no.nav.helse.domene.aktør
 
 import no.nav.helse.Feilårsak
-import no.nav.helse.oppslag.aktør.AktørregisterClient
 import no.nav.helse.domene.AktørId
+import no.nav.helse.domene.Fødselsnummer
+import no.nav.helse.oppslag.aktør.AktørregisterClient
 
 class AktørregisterService(private val aktørregisterClient: AktørregisterClient) {
 
     fun fødselsnummerForAktør(aktørId: AktørId) =
             aktørregisterClient.gjeldendeNorskIdent(aktørId.aktor).mapLeft {
+                Feilårsak.IkkeFunnet
+            }
+    fun aktørForFødselsnummer(fødselsnummer: Fødselsnummer) =
+            aktørregisterClient.gjeldendeAktørId(fødselsnummer.value).mapLeft {
                 Feilårsak.IkkeFunnet
             }
 }
